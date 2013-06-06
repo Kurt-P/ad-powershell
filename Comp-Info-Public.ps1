98a9f4e2cc8f125796b26b9768a2f254d93b46e3
Levi Webster <webst153@crk.umn.edu>
2013-06-06 10:41:55 -0500

    AD Powershell Scripts

---
+++ Comp-Info-Public.ps1
@@ -0,0 +1,16 @@
+﻿########################################################################
+#Name: Comp-Info.ps1                                                   #
+#About: Use to get computer information from AD                        #
+#       Displays Name, OperatingSystem,LastLoginDate                   #
+#Author: Levi Webster                                                  #
+#Date/Time: 924 06062013                                               #
+########################################################################
+
+#$searchBase is the OU OU information EX: OU=Computers,OU=Someplace,DC=ad,DC=something,DC=edu
+#edit searchbase for your ad structure
+$searchBase = '<YOUR OU HERE>'
+
+#Gather the Name, OperatingSystem, LastLoginDate
+$computerNames = Get-ADComputer -Filter {Name -like "umc-*"} -Properties Name,OperatingSystem -SearchBase "$searchBase" | Select-Object Name,OperatingSystem,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}
+#Export to CSV
+$computerNames | Export-Csv 'C:\Users\Public\compName.csv'
---
+++ ad-bulk-remove-comp-Public.ps1
@@ -0,0 +1,11 @@
+﻿#######################################################################################
+#Title: ad-bulk-remove-comp.ps1                                                       #
+#Description: Removes computers from ad that are in a list.txt file.                  #
+#             You must have a list of computer names for the script to work.          #
+#             I used 8470.txt for this one. Modify that file for your use.            #
+#             Use the -whatif at the end to see the results before using in prod      #
+#             Use at your own risk                                                    #
+#Author/Date: Levi Webster 1027 06062013                                              #
+#######################################################################################
+
+Get-Content "C:\users\Public\8470.txt" | % { Get-ADComputer -Filter { Name -eq $_ } } | Remove-ADComputer 
