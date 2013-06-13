17ade971536a88aca71378d236a11701b9f93e37
Levi Webster <webst153@crk.umn.edu>
2013-06-13 08:41:36 -0500

    New script for grouping objects and counting a value!

--- README.md
+++ README.md
@@ -17,3 +17,10 @@ Description:
 Use this script to query AD and get a list of computer names, the operating system, and last login date. 
 Currently there is a bug with the last login date but I hope to get that fixed soon.
 Details on usage can be found in the scripts comments. 
+
+#######################
+#Unique-Group-Count.ps1
+#######################
+Description:
+This script groups items in an array and then will sum create a sum of an item in the group.
+It is currently used for counting pages printed per user. It groups by user than totals the pages.
---
+++ Unique-Group-Count.ps1
@@ -0,0 +1,25 @@
+﻿#######################################################
+#Group unique objects in an array by -Unique property #
+#                                                     #
+#Author: Levi Webster @ 830 06132013                  #
+#######################################################
+
+#$gdata pulls grouped data from a printlog. Group-Object does the work and groups by userId
+$gdata = $printsLogs | Group-Object -Property userId
+
+#Array used to store values
+$test = @()
+
+# This does the work.
+#-Unique userId,@{Name = 'PageTotal'} selects the Columns
+#The @{Name = 'PageTotal'} creates a new column
+#The Expression is what is used to populate the PageTotal. In this case it is pages from a print log
+#The final result is the total sum of printed document per unique user
+$test += foreach($item in $gdata){
+
+    $item.Group | Select -Unique userId,
+    @{Name = 'PageTotal';Expression = {(($item.Group) | measure -Property pages -sum).Sum}}
+
+}
+
+###End###
